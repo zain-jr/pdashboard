f7526bc06aa4af5784b167182ef36a4bb08863c3
zain-jr <zainjrtech21@gmail.com>
2016-05-06 18:01:37 +0500

    Responsive

--- markup/js/dashboard.js
+++ markup/js/dashboard.js
@@ -1,3 +1,11 @@
 $(document).on('click', '.sideBar-opener', function(){
    $('body').toggleClass('sideBar-active'); 
 });
+
+$(document).on('click', '.slide-onMobile-opener', function(){
+   $('.property-listing-holder').toggleClass('slide-onMobile-active'); 
+});
+
+$(document).on('click', '.slide-onMobile > li > a', function(){
+   $('.property-listing-holder').removeClass('slide-onMobile-active'); 
+});

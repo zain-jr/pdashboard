47a6eb978a374ef8ad3e47028a653367e3ace1b4
zain-jr <zainjrtech21@gmail.com>
2016-06-28 06:38:16 +0500

    alignment

--- markup/js/customSelect.js
+++ markup/js/customSelect.js
@@ -8,7 +8,16 @@ $(document).on('change', '.custom_select_radio', function(){
  $(document).on('click', '.searchValue', function(event){
 	event.stopPropagation();
     
-	$('.searchAbleCustom-select').removeClass('active');
+	// $('.searchAbleCustom-select').removeClass('active');
+    // var elementFromTop = $(this).closest('.searchAbleCustom-select').position().top;
+    // var windowHeight = $(window).height();
+    // var elementFromBottom = windowHeight - elementFromTop;
+    // console.log(elementFromTop)
+    // console.log('windowHeight: '+windowHeight)
+    // console.log('elementFromBottom: '+elementFromBottom)
+    // if(elementFromBottom < 150){
+    //     alert();
+    // }
 	$(this).closest('.searchAbleCustom-select').toggleClass('active');
  });
   $(document).on('click', '.searchAbleCustom-select', function(event){
